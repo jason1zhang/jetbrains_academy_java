b171483f9539cc110b4b6f4b63ff3e8437492eb2
jason1zhang <jason1_zhang@hotmail.com>
2022-02-16 07:58:52 +0800

    Create ReverseString.java
    A recursive method which prints an input string in the reverse order.

---
+++ ReverseString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Recursion print strings in the reverse order
+ * @author Jason Zhang
+ * @version 1.0
+ * @since 2022-02-16
+ *  
+ * Given a recursive method which should print an input string in the reverse order.
+ */
+
+public class ReverseString {
+
+    /* Fix this method */
+    public static void printReverseCharByChar(String s) {
+        if (s.length() > 0) {
+            int last = s.length() - 1;
+            System.out.print(s.charAt(last));
+            printReverseCharByChar(s.substring(0, last));
+        }
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        printReverseCharByChar(scanner.nextLine());
+    }
+}
