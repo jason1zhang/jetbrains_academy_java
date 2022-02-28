d8779ddce20a019bae0bcb520b31b2174e598e84
jason1zhang <jason1_zhang@hotmail.com>
2022-03-01 03:39:39 +0800

    Update MethodDemo.java
    add demo for getClass

--- MethodDemo.java
+++ MethodDemo.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.Date;
 
 /**
- * This program is to demonstrate the usage of various methods in the java
- * system classes.
+ * This program is to demonstrate the usage of various methods in the java build-in classes.
  * 
  * @author Jason Zhang
  * @version 1.0
@@ -15,18 +15,22 @@ public class MethodDemo {
         boolean isDone = false;
 
         do {
-            System.out.print("Enter the method to demonstrate: > ");
+            System.out.print("\nEnter the method to demonstrate: > ");
             strMethod = scanner.next();
 
             switch (strMethod) {
                 case "nextLine":
                     nextLineDemo(scanner);
                     break;
-                case "done":
+                case "getClass":
+                    getClassDemo();
+                    break;
+                case "none":
                     isDone = true;
+                    System.out.println("\n");
                     break;
                 default:
-                    System.out.println("Unexpected method name, please enter again!\n");
+                    System.out.println("Unexpected method, please try to enter again!\n");
                     break;
             }
         } while (!isDone);
@@ -35,8 +39,7 @@ public class MethodDemo {
     }
 
     /**
-     * This method is to demonstrate the difference between the method next() and
-     * nextLine().
+     * demo the difference between the method next() and nextLine().
      * 
      * @param scanner java.util.Scanner
      */
@@ -51,6 +54,109 @@ public class MethodDemo {
         System.out.print("nextLine(): > ");
         scanner.nextLine(); // Important! Here need to call nextLine() one extra line to get rid of the carriage return
         String s2 = scanner.nextLine();
-        System.out.printf("s2 -> (%s)\n\n", s2);
+        System.out.printf("s2 -> (%s)\n\n\n", s2);
     }
+
+    /**
+     * demo the usage of getClass method
+     */
+    public static void getClassDemo() {
+        System.out.println("*** This is to demo the usage of method \"getClass()\" in the class Object. ***\n");
+
+        System.out.println("*** 1. using the subclass reference ***\n");
+
+        Person person = new Person("person 1");
+        Client client1 = new Client("client 1");
+        Employee employee1 = new Employee("employee 1");
+
+        System.out.printf("Class of ojbect {%s} is {%s}\n", person.toString(), person.getClass().getName());
+        System.out.printf("Class of ojbect {%s} is {%s}\n", client1.toString(), client1.getClass().getName());
+        System.out.printf("Class of ojbect {%s} is {%s}\n", employee1.toString(), employee1.getClass().getName());
+
+        System.out.println("\n\n*** 2. using the superclass reference ***");
+
+        Person client2 = new Client("client 2");
+        Person employee2 = new Employee("employee 2");
+
+        System.out.printf("Class of ojbect {%s} is {%s}\n", client2.toString(), client2.getClass().getName());
+        System.out.printf("Class of ojbect {%s} is {%s}\n\n", employee2.toString(), employee2.getClass().getName());
+    }
+}
+
+class Person {
+    protected String name;
+    protected int yearOfBirth;
+    protected String address;
+
+    public Person (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    } 
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
+
+class Client extends Person {
+    protected String contractNumber;
+    protected boolean gold;
+
+    public Client(String name) {
+        super(name);
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+    public boolean isGold() {
+        return gold;
+    }
+    public void setGold(boolean gold) {
+        this.gold = gold;
+    }    
+}
+
+class Employee extends Person {
+    protected Date startDate;
+    protected long salary;
+    
+    public Employee(String name) {
+        super(name);
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public long getSalary() {
+        return salary;
+    }
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }    
 }
