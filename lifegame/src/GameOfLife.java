1c0a580d38767517d9d9de7eb629e02b85a22add
jason1zhang <jason1_zhang@hotmail.com>
2022-05-24 20:36:32 +0800

    Update GameOfLife.java
    Completed the 5th stage of project "Game of Life"
       - added the restart functionality
       - passed all the tests

--- lifegame/src/GameOfLife.java
+++ lifegame/src/GameOfLife.java
@@ -3,59 +3,51 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GameOfLife extends JFrame{
-    private JLabel geneationLabel;
-    private JLabel aliveLabel;
-
-    private JToggleButton buttonPlayToggle;
-    private boolean isPaused;
-
-    private UniverseEvolution universeEvolution;
-
-    private JButton buttonReset;
-    private boolean isReset;
-
     private final static int WIDTH = 1200;
     private final static int HEIGHT = 900;
 
+    private JLabel geneationLabel;
+    private JLabel aliveLabel;
+
+    private JButton buttonReset;
+    private JToggleButton buttonPlayToggle;
+
+    private boolean isPaused;
     private CellPanel cellPanel;
-    private Universe universe;    
+    private UniverseEvolution universeEvolution;
+
+    private Universe universe;
     private int generation;
 
     public static void main(String[] args) throws InterruptedException {
         SwingUtilities.invokeLater(GameOfLife::new);
-    }      
+    }
 
     public GameOfLife() {
-
         super("Game of Life");
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout(2, 2));
-
         setResizable(false);
 
         // set up control panel
         JPanel controlPanel = new JPanel(new BorderLayout(2, 2));
         controlPanel.setSize(WIDTH/4, HEIGHT/4);
-        
 
         // set up button panel
         JPanel buttonPanel = new JPanel(new BorderLayout(2, 2));
         buttonPanel.setSize(WIDTH/4, HEIGHT/16);
 
-        this.buttonPlayToggle = new JToggleButton("Pause");
+        this.buttonPlayToggle = new JToggleButton("Resume");
         this.buttonPlayToggle.setName(("PlayToggleButton"));
-        actOnToggleButton();        
+        actOnToggleButton();
 
-        this.buttonReset = new JButton("Restart");
+        this.buttonReset = new JButton("Reset");
         this.buttonReset.setName("ResetButton");
         this.buttonReset.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                isReset = !isReset;
-                System.out.println("Start Button pressed!");
-
-                startGame();
+                universeEvolution.interrupt();
             }
         });
 
@@ -64,8 +56,7 @@ public class GameOfLife extends JFrame{
         controlPanel.add(buttonPanel, BorderLayout.NORTH);
 
         // set up labels panel
-        // JPanel labelPanel = new JPanel(new BorderLayout(5, 5));        
-        JPanel labelPanel = new JPanel();        
+        JPanel labelPanel = new JPanel();
         labelPanel.setSize(WIDTH/4, HEIGHT/16);
 
         this.geneationLabel = new JLabel();
@@ -82,38 +73,31 @@ public class GameOfLife extends JFrame{
         this.aliveLabel.setHorizontalAlignment(SwingConstants.LEFT);
         this.aliveLabel.setBounds(50, 50, WIDTH/4, 50);
 
-        // labelPanel.add(this.geneationLabel, BorderLayout.NORTH);
-        // labelPanel.add(this.aliveLabel, BorderLayout.CENTER);
         labelPanel.add(this.geneationLabel);
         labelPanel.add(this.aliveLabel);
         controlPanel.add(labelPanel, BorderLayout.CENTER);
         add(controlPanel, BorderLayout.WEST);
 
-        this.isPaused = false;
-        this.isReset = false;
+        this.isPaused = true;
 
         // set up cell panel
-
         this.universe = new Universe();
         this.generation = 1;
         setLabels();
-        
+
         cellPanel = new CellPanel(this.universe);
         cellPanel.setSize(new Dimension(getWidth() - controlPanel.getWidth(), getHeight()));
         add(cellPanel, BorderLayout.CENTER);    // Important: do NOT set to BorderLayout.EAST
         setVisible(true);
 
         universeEvolution = new UniverseEvolution();
-
         universeEvolution.start();
     }
 
-
     private class UniverseEvolution extends Thread {
-
         public void resumeThread() {
             synchronized(this) {
-               notify();
+                notify();
             }
         }
 
@@ -131,14 +115,16 @@ public class GameOfLife extends JFrame{
                     generation++;
                     cellPanel.setUniverse(universe);
 
-                    Thread.sleep(2000L);
+                    Thread.sleep(150L);
                     setLabels();
                     cellPanel.repaint();
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    universe = new Universe();
+                    generation = 1;
+                    setLabels();
                 }
-            }              
-        }        
+            }
+        }
     }
 
     private void setLabels() {
@@ -152,34 +138,16 @@ public class GameOfLife extends JFrame{
                 int state = itemEvent.getStateChange();
 
                 if (state == ItemEvent.SELECTED) {
-                    isPaused = true;
-                       buttonPlayToggle.setText("Resume");                    
-                } else {
                     isPaused = false;
                     buttonPlayToggle.setText("Pause");
                     universeEvolution.resumeThread();
+                } else {
+                    isPaused = true;
+                    buttonPlayToggle.setText("Resume");
                 }
             }
         };
 
         this.buttonPlayToggle.addItemListener(ItemListener);
-    }
-
-    public void startGame() {
-        // int LIMIT = 20;
-        System.out.println("Starting......game!");
-        while (this.isReset == true) {
-            repaint();           
-
-            System.out.println("Starting......startgame() -> repaint!");
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            this.universe = this.universe.generateNext();
-        }
     }
 }
