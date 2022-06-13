23bd77424b2394f2acb5a3cf67d7f545fa7d0e74
jason1zhang <jason1_zhang@hotmail.com>
2022-06-13 22:06:02 +0800

    minor updates

--- webquiz/src/main/java/engine/webquiz/WebQuizController.java
+++ webquiz/src/main/java/engine/webquiz/WebQuizController.java
@@ -1,7 +1,6 @@
 package engine.webquiz;
 
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,22 +15,21 @@ public class WebQuizController {
     @Autowired
     private WebQuizService service;
 
-    @GetMapping("/quiz")
+    @GetMapping("/api/quiz")
     public WebQuiz getWebQuiz() {
         return this.service.getQuiz();
     }
 
-    @PostMapping("/quiz")
-    public ResponseEntity<?> getFeedback(@RequestParam(required = true) String answer) {
+    @PostMapping("/api/quiz")
+    public ResponseEntity<?> getFeedback(@RequestParam(required = true) int answer) {
         LinkedHashMap<String, Object> status = new LinkedHashMap<>();
-        if (answer.equals("2")) {            
+        if (answer == 2) {
             status.put("success", true);
             status.put("feedback", "Congratulations, you're right!");
-            return new ResponseEntity<>(status, HttpStatus.OK);
         } else {
             status.put("success", false);
             status.put("feedback", "Wrong answer! Please, try again.");
-            return new ResponseEntity<>(status, HttpStatus.BAD_REQUEST);
         }
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 }
--- webquiz/src/main/java/engine/webquiz/WebQuizService.java
+++ webquiz/src/main/java/engine/webquiz/WebQuizService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WebQuizService {
-    private WebQuiz quiz;
+    private final WebQuiz quiz;
 
     public WebQuizService() {
         ArrayList<String> options = new ArrayList<>();
